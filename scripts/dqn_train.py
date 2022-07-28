86bea775c00b2aa62fc34e0dab4c69822f44713e
Keitaro Murakami <keitaro.mrkm@gmail.com>
2022-07-28 18:48:07 +0900

    <feat>[dqn_agent.py] hyperparameters

--- scripts/dqn_train.py
+++ scripts/dqn_train.py
@@ -18,8 +18,13 @@ import torch.optim as optim
 
 
 def train(hyperparams: dict):
-    print(args)
-    agant = DQNAgent()
+
+    state_size = 26
+    action_size = 5
+
+    agent = DQNAgent(state_size, action_size, hyperparams, seed=0)
+
+    return agent
 
 
 if __name__ == "__main__":
@@ -30,18 +35,21 @@ if __name__ == "__main__":
         del sys.argv[-2:]
     ##
 
-    parser.add_argument('--batch_size', default=64)
-    parser.add_argument('--gamma', default=0.995)
-    parser.add_argument('--tau', default=1e-3)
-    parser.add_argument('--lr', default=0.1)
-    parser.add_argument('--every_update', default=4)
+    parser.add_argument('--batch_size', type=int, default=64)
+    parser.add_argument('--gamma', type=float, default=0.995)
+    parser.add_argument('--tau', type=float, default=1e-3)
+    parser.add_argument('--lr', type=float, default=0.1)
+    parser.add_argument('--every_update', type=int, default=4)
 
-    parser.add_argument('--n_episodes', default=3000)
-    parser.add_argument('--max_t', default=1000)
-    parser.add_argument('--eps_start', default=1.0)
-    parser.add_argument('--eps_end', default=0.01)
-    parser.add_argument('--eps_decay', default=0.995)
+    parser.add_argument('--n_episodes', type=int, default=3000)
+    parser.add_argument('--max_t', type=int, default=1000)
+    parser.add_argument('--eps_start', type=float, default=1.0)
+    parser.add_argument('--eps_end', type=float, default=0.01)
+    parser.add_argument('--eps_decay', type=float, default=0.995)
 
     args = vars(parser.parse_args())
 
     agent = train(hyperparams=args)
+
+    """ for debug (later delete) """
+    print(agent.get_hyperparams())
--- src/line_trace_dqn/dqn_agent.py
+++ src/line_trace_dqn/dqn_agent.py
@@ -12,8 +12,50 @@ import torch.optim as optim
 
 class DQNAgent():
 
-    def __init__(self):
-        pass
+    """Interacts with and learns form environment."""
+    def __init__(self, state_size, action_size, optional_hyperparams: dict = {}, seed=0):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.seed = seed
+
+
+        """ hyperparameters """
+        self.hyperparams = ['batch_size', 'gamma', 'tau', 'lr', 'every_update', 'n_episodes', 'max_t', 'eps_start', 'eps_end', 'eps_decay']
+        self.buffer_size = int(1e5) # replay buffer size
+        self.batch_size = 64        # minibatch size, or how many samples taken the replay buffer for experience replay
+        self.gamma = 0.995          # discount factor
+        self.tau = 1e-3             # for soft update of target parameters
+        self.lr = 0.1               # learning rate
+        self.every_update = 4       # # how often to update the network
+        self.n_episodes = 3000      # maximum number of training epsiodes
+        self.max_t = 1000           # maximum number of timesteps per episode
+        self.eps_start = 1.0        # starting value of epsilon, for epsilon-greedy action selection
+        self.eps_end = 0.01         # minimum value of epsilon
+        self.eps_decay = 0.995      # mutiplicative factor (per episode) for decreasing epsilon
+        self.set_hyperparams(optional_hyperparams)
+
+
+    def set_hyperparams(self, param_dict: dict):
+        """setter for hyperparamters
+
+        Args:
+            param_dict (dict): parameter {param_name: param_value}
+        """
+        for k, v in param_dict.items():
+            assert hasattr(self, k), "no such hyperparamter named '{}'".format(k)
+            setattr(self, k, v)
+
+
+    def get_hyperparams(self):
+        """getter for hyperparameters
+
+        Returns:
+            dict: param_dict {param_name: param_value}
+        """
+        param_dict = {}
+        for key in self.hyperparams:
+            param_dict[key] = getattr(self, key)
+        return param_dict
 
 
 if __name__ == "__main__":
